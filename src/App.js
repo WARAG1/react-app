6d03b84e3cc1a3a0a7cbf6534ca6122e4987ab49
Kirill Novykh <kirillnovykh486@gmail.com>
2022-04-18 23:19:38 +0500

    11:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/ru/thumb/5/5f/NAVI_Logo.svg/1163px-NAVI_Logo.svg.png' />
       </header>
+      <nav className='nav'>
+        <div>
+          <a href='#'>Profile</a>
+        </div>
+        <div>
+          <a href='#'>News</a>
+        </div>
+        <div>
+          <a href='#'>Messages</a>
+        </div>
+        <div>
+          <a href='#'>Music</a>
+        </div>
+      </nav>
+      <div className='content'>
+
+      </div>
     </div>
   );
 }
