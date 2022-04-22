715c9c31c593547bf182daeff39d17c680fc6f72
Kirill Novykh <kirillnovykh486@gmail.com>
2022-04-22 18:08:14 +0500

    15

--- src/components/Profile/MyPost/NewPost/MyPost.js
+++ src/components/Profile/MyPost/NewPost/MyPost.js
@@ -4,22 +4,24 @@ import classes from './MyPost.module.css'
 const MyPost = () => {
   return (
     <div>
-    <div>
       <div>
-        My posts
-      </div>
-      <div className={classes.post}>
         <div>
-          New post
+          My posts
         </div>
-        <div className={classes.item}>
-          Post 1
-        </div>
-        <div className={classes.item}>
-          Post 2
+        <div className={classes.post}>
+          <div>
+            <textarea></textarea>
+            <button>Add Text</button>
+          </div>
+          <div className={classes.item}>
+            <img src='https://svirtus.cdnvideo.ru/uxZDrOgTmlSIXbf3xdMg96HzCco=/0x0:429x429/200x200/filters:quality(100)/https://hb.bizmrg.com/esports-core-media/45/45063cdab3574988ea1cd9f2584cd5f9.jpg?m=49897f93c30f01784d2b62df9800dbd9'></img>
+            Post 1
+          </div>
+          <div className={classes.item}>
+            Post 2
+          </div>
         </div>
       </div>
-    </div>
     </div >
   )
 }
