af7a8f7db3972f36da55f018b47b5580d032ea40
Kirill Novykh <kirillnovykh486@gmail.com>
2022-04-25 15:57:51 +0500

    19

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './Dialogs.module.css'
+
+const Dialogs = () => {
+    return (
+        <div>
+            Dialogs
+        </div>
+
+
+    )
+
+}
+
+export default Dialogs
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import classes from './Profile.module.css'
 const Profile = () => {
   return (
     <div>
-    <div className={classes.content}>
+    <div >
       <div>
         <img src='https://krot.info/uploads/posts/2021-02/1613711650_1-p-navi-fon-na-rabochii-stol-1.jpg' width="1450" height="250"></img>
       </div>
