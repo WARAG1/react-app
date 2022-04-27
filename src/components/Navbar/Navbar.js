9327caae80d49ec5553a75bd8a95dbfd1201a46f
Kirill Novykh <kirillnovykh486@gmail.com>
2022-04-27 16:57:55 +0500

    20

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
-        <a>Profile</a>
+        <a href='/profile'>Profile</a>
+      </div>
+      <div className={classes.item}>
+        <a href='/dialogs'>Messages</a>
       </div>
       <div className={classes.item}>
         <a>News</a>
       </div >
       <div className={classes.item}>
-        <a>Messages</a>
-      </div>
-      <div className={classes.item}>
         <a>Music</a>
       </div>
     </nav>
   );
-}
+} 
 
 export default Navbar;
